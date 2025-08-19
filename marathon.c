af8c4f71cd59074961928ec068f0a6d2d74a65de
vagdodaritu <vagdodaritu@gmail.com>
2025-08-19 10:59:47 +0530

    marathon

---
+++ marathon.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    float time,distance_covered,minute,start_km,end_km;
+    printf(" Enter the distance_covered by runner in every minute");
+    scanf("%d",&distance_covered);
+    {
+        if(start_km=0 && end_km=20)
+        {
+            printf(" distance_covered im every 1 minute is 0.5km ");
+        }
+    }
+    printf(" Time tahen to complete marathon is 20 minute");
+    printf("complete marathon");
+}
