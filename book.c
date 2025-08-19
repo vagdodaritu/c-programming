14e1f33bbc5d1ce061a1633b323e925429941925
vagdodaritu <vagdodaritu@gmail.com>
2025-08-19 10:20:06 +0530

    book

---
+++ book.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x,N;
+    scanf("%d,&N");
+    for( x=1; x<=50; x=x+1);
+    printf(" Book ID:%d,x");
+    if( x %=5)
+    {
+        printf(" special edition ");
+    }
+    return 0;
+
+}
+
