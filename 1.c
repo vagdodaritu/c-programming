13876b64f0e8c0386be6dbacf6267ecaec7eab56
vagdodaritu <vagdodaritu@gmail.com>
2025-09-01 23:48:34 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf("Vagdoda Ritu Bhikhabhai\n");
+    printf("Age=19\n");
+    printf("College name is Charusat university of science and tachnology\n");
+    return 0;
+}
