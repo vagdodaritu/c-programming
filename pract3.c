36f2d24330a4dda4179c4e18426489459408c4a2
vagdodaritu <vagdodaritu@gmail.com>
2025-11-06 15:42:34 +0530

    Add files via upload

---
+++ pract3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    float height,weight,bmi;
+    printf("Name: Ritu Vagdoda\nID Number:25CE128");
+    printf("Enter the value of height");
+    scanf("%d",&height);
+    printf("Enter the value of weight");
+    scanf("%d",&weight);
+    bmi=weight/(height*height);
+    printf("Body mass index is %f\n");
+    if(bmi<18.5)
+    {
+        printf("category:Underweight\n");
+    }
+    else if(bmi>=18.5 && bmi<24.9)
+    {
+        printf("category:Normalweight\n");
+    }
+    else if(bmi>=25 && bmi<29.9)
+    {
+        printf("category:Overweight\n");
+    }
+    else
+    {
+        printf("category:obesse\n");
+    }
+    return 0;
+}
